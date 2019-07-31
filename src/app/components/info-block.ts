1768c8c7819615ec70490222d4f7127a4b0e74ca
gordon <gordon.from.blumberg@gmail.com>
2019-07-31 04:31:45 +0400

    Create InfoBlock and use it as play button on main menu

--- src/app/components/info-block.ts
+++ src/app/components/info-block.ts
@@ -1,78 +1,250 @@
 import * as PIXI from 'pixi.js-legacy';
 
-import { ISize } from './isize';
 import { Resources } from '../util/resources';
+import { publicDecrypt } from 'crypto';
 
-export type BackgroundOptions = {
-  texture: string;
-  useTiling?: boolean;
-  size?: ISize;
-  padding?: ISize;
+type Size = {
+  x?: number;
+  y?: number;
+}
+
+type ComponentSizing = {
+  component: PIXI.Container;
+  size?: Size;
+  margin?: Size;
 }
 
 export class InfoBlock extends PIXI.Container {
 
-  private bgSprite: PIXI.Sprite;
+  private bgSprites: PIXI.Sprite[] = [];
+  private bgGraphics: PIXI.Graphics;
   private textComponent: PIXI.Text;
+  private iconComponent: PIXI.Sprite;
 
-  private fixedWidth = false;
-  private padding: ISize;
+  private size: Size;
+  private padding: Size = { x: 0, y: 0 };
 
-  constructor(bgOptions: BackgroundOptions, text: string, textStyle?: PIXI.TextStyle) {
-    super();
+  private layoutMode: 'padding' | 'size' = 'padding';
 
-    if (bgOptions.size && bgOptions.size.x) {
-      this.fixedWidth = true;
+  private componentsSizing: ComponentSizing[] = [];
+
+  /**
+   * @param textures Names of textures.
+   * If one - it will be used for whole block
+   * If two - the first will be at left and reflected at right, 
+   *    the second will be at center
+   * If three - they will be used from left to right
+   * @param useTilingSprite Flag to determine what class (TilingSprite or Sprite)  
+   * to use for the central background sprite
+   */
+  background(textures: string[] | string | PIXI.Graphics, useTilingSprite?: boolean): InfoBlock {
+    let bgTextures: string[];
+    if (typeof textures === 'string') {
+      bgTextures.push(textures);
+    } else if (textures instanceof Array) {
+      bgTextures = textures;
+    } else {
+      this.bgGraphics = textures;
+      this.addChild(this.bgGraphics);
+      return this;
     }
 
-    if (bgOptions.padding) {
+    if (bgTextures.length === 1) {
+
+      const bgSprite = this.createCenterBgSprite( Resources.get(bgTextures[0]), useTilingSprite );
+      
+      this.bgSprites.push(bgSprite);
+
+    } else {
+
+      this.bgSprites.push( new PIXI.Sprite( Resources.get(bgTextures[0]) ));
+      this.bgSprites.push( this.createCenterBgSprite( Resources.get(bgTextures[1]), useTilingSprite ));
+      this.bgSprites.push( this.createRightBgSprite(bgTextures) );
+
+    }
+
+    this.addChild(...this.bgSprites);
+
+    return this;
+  }
+
+  /**
+   * @param size Width and height of the center sprite. If not specified it is calculated automatically
+   */
+  setSize(size: Size): InfoBlock {
+    this.layoutMode = 'size';
+    this.size = size;
+    return this;
+  }
+
+  /**
+   * Sets padding for inner elements
+   * @param padding Is Size object is passed it will be used as padding. 
+   * If number is passed the value will be set to padding.x and to padding.y, 
+   * if y is not specified
+   * @param y Value for padding.y
+   */
+  setPadding(padding: number | Size, y?: number): InfoBlock {
+    this.layoutMode = 'padding';
+
+    if (typeof padding === 'number') {
+      this.padding = { 
+        x: padding, 
+        y: y != undefined ? y : padding //y may be 0, so y || padding is not correct
+      };
+    } else {
       this.padding = {
-        x: bgOptions.padding.x,
-        y: bgOptions.padding.y
+        x: padding.x || 0,
+        y: padding.y || 0
       };
     }
 
-    const bgTexture = Resources.get(bgOptions.texture);
-    this.bgSprite = bgOptions.useTiling
-        ? new PIXI.TilingSprite(bgTexture, bgOptions.size.x, bgOptions.size.y)
-        : this.createSimpleSprite(bgTexture, bgOptions.size);
-    this.addChild(this.bgSprite);
+    return this;
+  }
 
-    this.textComponent = new PIXI.Text(text, textStyle);
-    this.textComponent.position.set(
-      this.padding.x,
-      this.padding.y
-    );
+  addIcon(texture: string, margin?: Size, size?: Size): InfoBlock {
+    this.iconComponent = new PIXI.Sprite( Resources.get(texture) );
+
+    this.componentsSizing.push({
+      component: this.iconComponent,
+      margin: this.getSizeSafe(margin),
+      size: this.getSizeSafe(size)
+    });
+
+    this.addChild(this.iconComponent);
+
+    return this;
+  }
+
+  addText(text: string, margin?: Size, style?: PIXI.TextStyle): InfoBlock {
+    this.textComponent = new PIXI.Text(text, style);
+    
+    this.componentsSizing.push({
+      component: this.textComponent,
+      margin: this.getSizeSafe(margin)
+    });
+
     this.addChild(this.textComponent);
 
-    this.adjustSize();
+    return this;
   }
 
-  setText(text: string): ISize {
+  setButtonMode(buttonMode: boolean): InfoBlock {
+    this.interactive = buttonMode;
+    this.buttonMode = buttonMode;
+    return this;
+  }
+
+  finishBuild(): InfoBlock {
+    this.recalculate();
+
+    return this;
+  }
+
+  setText(text: string, recalculate: boolean) {
     this.textComponent.text = text;
-    this.adjustSize();
-    return { x: this.width, y: this.height };
-  }
-
-  setTextStyle(style: PIXI.TextStyle): ISize {
-    this.textComponent.style = style;
-    this.adjustSize();
-    return { x: this.width, y: this.height };
-  }
-
-  private createSimpleSprite(texture: PIXI.Texture, size?: ISize) {
-    const sprite = new PIXI.Sprite(texture);
-    if (size) {
-      if (size.x) sprite.width = size.x;
-      if (size.y) sprite.height = size.y;
+    if (recalculate) {
+      this.recalculate();
     }
-    return sprite;
   }
 
-  private adjustSize() {
-    if (this.fixedWidth) return;
+  private createCenterBgSprite(texture: PIXI.Texture, useTilingSprite?: boolean): PIXI.Sprite {
+    return useTilingSprite 
+        ? new PIXI.TilingSprite(texture)
+        : new PIXI.Sprite(texture);
+  }
 
-    this.width = this.textComponent.width + 2 * this.padding.x;
-    this.height = this.textComponent.height + 2 * this.padding.y;
+  private createRightBgSprite(bgTextures: string[]): PIXI.Sprite {
+    if (bgTextures.length === 3) {
+      return new PIXI.Sprite( Resources.get(bgTextures[2]) );
+    }
+
+    const rightBgSprite = new PIXI.Sprite( Resources.get(bgTextures[0]) );
+    rightBgSprite.scale.x = -1;
+    return rightBgSprite;
+  }
+
+  //calculate size if layoutMode == 'padding'
+  //layoutMode == 'size' means size was set explicitly
+  //width is sum of paddings, inner elements widths and margins between them
+  //height is sum of paddings and max height of inner elements
+  private calculateSize() {
+    if (this.layoutMode == 'padding') {
+      this.size = {
+        x: 2 * this.padding.x,
+        y: 2 * this.padding.y
+      };
+      let maxHeight = 0;
+
+      this.componentsSizing.forEach(cs => {
+        const width = (cs.size && cs.size.x) || cs.component.width;
+        const height = (cs.size && cs.size.y) || cs.component.height;
+
+        this.size.x += cs.margin.x + width;
+        maxHeight = height > maxHeight ? height : maxHeight;
+      });
+
+      this.size.y += maxHeight;
+    }
+  }
+
+  private composeBackground() {
+    // if graphics is used as bg - set size to it
+    if (this.bgGraphics) {
+
+      this.bgGraphics.width = this.size.x;
+      this.bgGraphics.height = this.size.y;
+
+      // if bg is one sprite - set size to it
+    } else if (this.bgSprites.length === 1) {
+
+      this.bgSprites[0].width = this.size.x;
+      this.bgSprites[0].height = this.size.y;
+
+    } else {
+      
+      let x = 0;
+      // if bg consists of three sprites - sizes of left and right leave default,
+      // size of center calculate
+      this.bgSprites[1].width = this.size.x - this.bgSprites[0].width - this.bgSprites[2].width;
+
+      this.bgSprites.forEach(bgSprite => {
+        bgSprite.position.x = x;  // place sprites close to each other
+        x += bgSprite.width;
+        bgSprite.height = this.size.y; // set the same height for each
+      });
+
+    }
+  }
+
+  // todo: composing correctly works only for 'padding' mode
+  private composeInnerElements() {
+    let x = this.padding.x;
+    this.componentsSizing.forEach(cs => {
+      if (cs.size && cs.size.x) {
+        cs.component.width = cs.size.x;
+      }
+      if (cs.size && cs.size.y) {
+        cs.component.height = cs.size.y;
+      }
+
+      x += cs.margin.x;
+      cs.component.position.set( x, (this.size.y - cs.component.height) / 2 );
+
+      x += cs.component.width;
+    });
+  }
+
+  private recalculate() {
+    this.calculateSize();
+    this.composeBackground();
+    this.composeInnerElements();
+  }
+
+  private getSizeSafe(size: Size): Size {
+    return {
+      x: size && size.x || 0,
+      y: size && size.y || 0
+    }
   }
 }
--- src/app/state/main-menu/main-menu-state.ts
+++ src/app/state/main-menu/main-menu-state.ts
@@ -3,6 +3,7 @@ import * as PIXI from 'pixi.js-legacy';
 import { AbstractState } from '../abstract-state';
 import { ConfigService } from '../../config/config-service';
 import { Resources } from '../../util/resources';
+import { InfoBlock } from '../../components/info-block';
 
 export class MainMenuState extends AbstractState {
   private readonly BUTTON_BORDER_COLOR = 0xa58830;
@@ -11,7 +12,7 @@ export class MainMenuState extends AbstractState {
   private config;
 
   private playButtonHovered = false;
-  private playButton: PIXI.Graphics;
+  private playButton: PIXI.Container;
 
   constructor(scene: PIXI.Container) {
     super('mainMenu', scene);
@@ -32,26 +33,16 @@ export class MainMenuState extends AbstractState {
     icon.position.set(this.scene.width - icon.width, this.scene.height - icon.height);
     this.scene.addChild(icon);
 
-    this.playButton = new PIXI.Graphics();
-    this.playButton.interactive = true;
-    this.playButton.buttonMode = true;
-    this.scene.addChild(this.playButton);
-    this.drawPlayButton(this.BUTTON_BORDER_COLOR);
-
     const playButtonCfg = mainMenuCfg.playButton;
-    const playIcon = new PIXI.Sprite(Resources.get(playButtonCfg.icon));
-    playIcon.position.set(
-      playButtonCfg.x + playButtonCfg.padding.x, 
-      playButtonCfg.y + playButtonCfg.padding.y
-    );
-    this.playButton.addChild(playIcon);
-
-    const playText = new PIXI.Text('PLAY', playButtonCfg.textStyle);
-    playText.position.set(
-      playIcon.getGlobalPosition().x + playIcon.width + playButtonCfg.iconTextSpace, 
-      playButtonCfg.y + playButtonCfg.padding.y
-    );
-    this.playButton.addChild(playText);
+    this.playButton = new InfoBlock()
+        .background( this.createPlayButtonBackground() )
+        .setPadding(playButtonCfg.padding)
+        .setButtonMode(true)
+        .addIcon(playButtonCfg.icon)
+        .addText('PLAY', { x: playButtonCfg.textMargin }, playButtonCfg.textStyle)
+        .finishBuild();
+    this.playButton.position.set(playButtonCfg.x, playButtonCfg.y);
+    this.scene.addChild(this.playButton);
 
     //todo: hightlight on hover does not work
     //this.playButton.on('mouseover', () => this.playButtonHovered = true);
@@ -61,24 +52,22 @@ export class MainMenuState extends AbstractState {
   }
   
   update(dtime: number, dms: number) {
-    const lineColor = this.playButton.line.color;
-    if (this.playButtonHovered && lineColor < this.BUTTON_BORDER_HOVERED_COLOR) {
-      this.drawPlayButton(lineColor + 0x010101);
+    // const lineColor = this.playButton.line.color;
+    // if (this.playButtonHovered && lineColor < this.BUTTON_BORDER_HOVERED_COLOR) {
+    //   this.drawPlayButton(lineColor + 0x010101);
 
-    } else if (!this.playButtonHovered && lineColor > this.BUTTON_BORDER_COLOR) {
-      this.drawPlayButton(lineColor - 0x010101);
-    }
+    // } else if (!this.playButtonHovered && lineColor > this.BUTTON_BORDER_COLOR) {
+    //   this.drawPlayButton(lineColor - 0x010101);
+    // }
   }
 
-  private drawPlayButton(lineColor: number) {
+  private createPlayButtonBackground(): PIXI.Graphics {
     const cfg = this.config.mainMenu.playButton;
-    this.playButton
+    return new PIXI.Graphics()
       .clear()
       .beginFill(0x80aaff)
-      .lineStyle(5, lineColor)
-      .drawRoundedRect(cfg.x, cfg.y, cfg.width, cfg.height, 20)
+      .lineStyle(5, 0xa58830)
+      .drawRoundedRect(0, 0, cfg.width, cfg.height, 20)
       .endFill();
-
-    this.scene.addChild(this.playButton);
   }
 }
